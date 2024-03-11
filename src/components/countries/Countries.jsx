ee5abc6edec000fdfa23675b268463816da84519
shahinexy <shahinexy@gmail.com>
2024-03-11 09:43:38 +0600

    get data from api

---
+++ src/components/countries/Countries.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Country from './../country/country';
+
+const Countries = () => {
+    const [countries, setCountries] = useState([])
+
+    useEffect(()=>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountries(data))
+    },[])
+
+    return (
+        <div>
+            <h2>Country: {countries.length}</h2>
+            {
+                countries.map(country => <Country key={country.cca3} country={country}></Country>)
+            }
+        </div>
+    );
+};
+
+export default Countries;
